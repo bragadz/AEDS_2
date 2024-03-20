e0ffc80fa39a432c9405a2682cc4b55696718744
bragadz <arthur.bracarvalho123@gmail.com>
2024-03-19 22:23:40 -0300

    novo commit

--- AEDS 2/BEECROWD/CombinarStrings.java
+++ AEDS 2/BEECROWD/CombinarStrings.java
@@ -2,7 +2,7 @@ package BEECROWD;
 
 import java.util.Scanner;
 
-public class Main {
+public class CombinarStrings {
 
     public static String combinarString(String str1, String str2){
         StringBuilder combinadas = new StringBuilder();
---
+++ AEDS 2/BEECROWD/SobreNome.java
@@ -0,0 +1,42 @@
+package BEECROWD;
+
+import java.util.Scanner;
+
+public class SobreNome {
+
+    public static boolean eHconsoantes(String palavra) {
+        String consoantes = "bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ";
+
+        int contador = 0;
+
+        for (int i = 0; i < palavra.length(); i++) {
+            char letra = palavra.charAt(i);
+            if (consoantes.indexOf(letra) != -1) {
+                contador++;
+                if (contador >= 3) {
+                    return false;
+                }
+            } else {
+                contador = 0;
+            }
+        }
+        return true;
+    }
+
+    public static void imprimirResultado(boolean resultado, String palavra) {
+        System.out.print(resultado ? palavra + " eh facil\n" : palavra + " nao eh facil\n");
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        sc.nextLine();
+
+        for (int i = 0; i < N; i++) {
+            String palavra = sc.nextLine();
+            imprimirResultado(eHconsoantes(palavra), palavra);
+        }
+        sc.close();
+    }
+}
